faae1f7849c901c1a24b0dc434b1babdf52b92af
jim-cassidy <jim-cassidy@outlook.com>
2020-01-03 00:31:46 -0500

    Create tkinter_sql.py

---
+++ module/tkinter_sql.py
@@ -0,0 +1,13 @@
+## Import necessary libraries
+
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+import numpy as np
+import matplotlib.pyplot as plt
+import sqlite3
+from sqlite3 import Error
+from tkinter import *
+import time
+import datetime
+from datetime import datetime
