25275aa1c8905a3216c195acc1e0309ed4f18b68
jim-cassidy <jim-cassidy@outlook.com>
2020-01-03 00:09:31 -0500

    Create tkin.py

---
+++ tkin.py
@@ -0,0 +1,11 @@
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+import numpy as np
+import matplotlib.pyplot as plt
+import sqlite3
+from sqlite3 import Error
+from tkinter import *
+import time
+import datetime
+from datetime import datetime
