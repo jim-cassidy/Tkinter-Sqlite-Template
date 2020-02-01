cc3ecc4789df0a00b3315a4ce7b2db717aa01252
jim-cassidy <jim-cassidy@outlook.com>
2020-02-01 01:22:22 +0000

    Update tkinter_sql.py

--- module/tkinter_sql.py
+++ module/tkinter_sql.py
@@ -1,3 +1,4 @@
+
 ## Import necessary libraries
 
 from sklearn.linear_model import Ridge
@@ -11,3 +12,22 @@ from tkinter import *
 import time
 import datetime
 from datetime import datetime
+
+class MyWindow:
+
+    def __init__(self, win):
+        self.label1=Label(win, text='Label1')
+        self.label2=Label(win, text='Label2')
+        self.entry1=Entry(bd=3)
+        self.entry2=Entry(bd=3)
+        self.label1.place(x=100, y=50)
+        self.label2.place(x=100, y=150)
+        self.entry1.place(x=200, y=50)
+        self.entry2.place(x=200, y=150)
+       
+
+window=Tk()
+mywin=MyWindow(window)
+window.title('Title goes here')
+window.geometry("800x600+10+10")
+window.mainloop()
