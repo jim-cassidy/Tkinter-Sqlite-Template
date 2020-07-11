40ffc286554594fa99d5009a5f71a965a9aee230
jim-cassidy <jim-cassidy@outlook.com>
2020-07-10 23:26:07 -0400

    Add files via upload

---
+++ module/main.py
@@ -0,0 +1,34 @@
+## We are going to be using numpy
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+## import scikit-learn
+
+import sklearn
+
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+## we will be using sqlite
+
+import sqlite3
+from sqlite3 import Error
+
+## here is our tkinter import
+
+from tkinter import *
+
+## here is the date and time function import
+
+import time
+import datetime
+
+   
+
+   
+
+ 
+
+
